564cadb08b0bb0f15f7f8740bab06f4eb178eb90
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-08-01 21:16:31 +0530

    custon input and custom button components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import signInAndSignUpPage from "./pages/signIn-and-signUp/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={signInAndSignUpPage} />
       </Switch>
     </div>
   );
