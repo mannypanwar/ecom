e16e980747a30538b66f49616a41ba7b7f255415
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-08-07 18:42:40 +0530

    saving user in state 2

--- src/App.js
+++ src/App.js
@@ -22,8 +22,19 @@ class App extends React.Component {
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
-        const userRef = createUserProfileDocument(user);
-        (await userRef).onSnapshot( snapshot =>  )
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
       }
     });
   }
